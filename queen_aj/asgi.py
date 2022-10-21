c1798590fbe6934b21fa74489de3d1f07c797671
Samuel wilson <sixtus99@gmail.com>
2022-10-21 05:22:02 +0000

    Nana nana blog app, first commit

---
+++ queen_aj/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for queen_aj project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'queen_aj.settings')
+
+application = get_asgi_application()
