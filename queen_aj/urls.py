2f0efd21b4795ff9a98802013dd08aebfd9fda66
Samuel wilson <sixtus99@gmail.com>
2022-10-21 11:37:12 +0000

    Modified templates to display posts from database.

--- queen_aj/urls.py
+++ queen_aj/urls.py
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django import http
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import include, path
 
-def Home(request):
-    return HttpResponse('home')
 
 
 
